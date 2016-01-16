e654257fdbb3ad51c0556f1042b0718d9deff521
Tim (robske_110) <robske110@users.noreply.github.com>
2016-01-16 16:28:29 +0100

    Added Simple PlayerJoin...
    Currently working on: Config enable/disable!

--- SRC/LightningOnDeath/Main.php
+++ SRC/LightningOnDeath/Main.php
@@ -5,6 +5,7 @@ namespace LightningOnDeath;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\entity\Entity;
@@ -13,13 +14,30 @@ use pocketmine\event\Listener;
 class Main extends PluginBase implements Listener{
 
 public function onLoad(){
-  $this->getLogger()->info("Preparing For StartUp STAND BY");
+  $this->getLogger()->info("Preparing For StartUp STAND BY"); //Useless
 }
 
 public function onEnable(){
   $this->getServer()->getPluginManager()->registerEvents($this, $this);
-  $this->getLogger()->info("LIGHTNING DEATH RUNNING");
+  $this->getLogger()->info("LIGHTNING STRIKES ARE READY!");
 }
+    public function PlayerJoin(PlayerJoinEvent $e){
+        $p = $e->getPlayer();
+        $level = $p->getLevel();
+        $light = new AddEntityPacket();
+        $light->type = 93;
+        $light->eid = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        foreach($level->getPlayers() as $pl){
+        $pl->dataPacket($light);
+        }
+    }
 
 public function onDeath(PlayerDeathEvent $ent){
 

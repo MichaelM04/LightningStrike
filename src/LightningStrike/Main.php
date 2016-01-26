4896c1fe51e00c6002a5beb6e9e4757f41ad0fb5
Michael <Mikeydoo4u@gmail.com>
2016-01-26 17:55:37 -0600

    Cleaned Up And Deleted Config (Didn't work) and deleted quit strike (also didn't work)

--- src/LightningStrike/Main.php
+++ src/LightningStrike/Main.php
@@ -6,7 +6,6 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\entity\Entity;
@@ -20,9 +19,6 @@ class LightningStrike extends PluginBase implements Listener{
       $this->getLogger()->info(TextFormat::AQUA."Preparing For StartUp... STAND BY");
     }
     public function onEnable(){
-      $this->saveDefaultConfig();
-      $this->lightning = $this->getConfig()->getAll();
-      $this->getServer()->getPluginManager()->registerEvents($this, $this);
       $this->getLogger()->info(TextFormat::GREEN."Lightning Strike RUNNING Made By MichaelM04");
     }
     /**
@@ -79,24 +75,6 @@ Server::broadcastPacket($level->getPlayers(),$light);
     	
     }
 
-    public function onQuit(PlayerQuitEvent $e){
-	$p = $e->getPlayer();
-	$light = new AddEntityPacket();
-        $light->type = 93;
-        $light->eid = Entity::$entityCount++;
-        $light->metadata = array();
-        $light->speedX = 0;
-        $light->speedY = 0;
-        $light->speedZ = 0;
-        $light->yaw = $p->getYaw();
-        $light->pitch = $p->getPitch();
-        $light->x = $p->x;
-        $light->y = $p->y;
-        $light->z = $p->z;
-      
-Server::broadcastPacket($level->getPlayers(),$light);
-    	
-    }
     public function onDeath(PlayerDeathEvent $e){
 
 $p = $e->getEntity();
